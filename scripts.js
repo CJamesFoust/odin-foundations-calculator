0d6b2f41c7751281de751b427765fc8151366375
CJamesFoust <82835548+CJamesFoust@users.noreply.github.com>
2025-07-28 20:44:43 -0400

    add operate function

--- scripts.js
+++ scripts.js
@@ -13,3 +13,18 @@ const multiply = (a, b) => {
 const divide = (a, b) => {
   return a / b;
 };
+
+const operate = (a, o, b) => {
+  switch (o) {
+    case "+":
+      return add(a, b);
+    case "-":
+      return subtract(a, b);
+    case "*":
+      return multiply(a, b);
+    case "/":
+      return divide(a, b);
+    default:
+      return console.error(`Sent value not expected: ${o}`);
+  }
+};
