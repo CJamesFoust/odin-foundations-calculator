79f9f1f2f63980485cdcc568fc11c81a235af021
CJamesFoust <82835548+CJamesFoust@users.noreply.github.com>
2025-07-29 01:22:02 -0400

    add major functionality. Plus, Minus, Multiply, Division, and Equals now work as expected. Front end properly displays with function firing

--- scripts.js
+++ scripts.js
@@ -1,17 +1,37 @@
 const add = (a, b) => {
-  return a + b;
+  firstInput.textContent = a + b;
+  operatorElement.textContent = "";
+  secondInput.textContent = "";
+  firstDigit = a + b;
+  secondDigit = 0;
+  operator = "";
 };
 
 const subtract = (a, b) => {
-  return a - b;
+  firstInput.textContent = a - b;
+  operatorElement.textContent = "";
+  secondInput.textContent = "";
+  firstDigit = a - b;
+  secondDigit = 0;
+  operator = "";
 };
 
 const multiply = (a, b) => {
-  return a * b;
+  firstInput.textContent = a * b;
+  operatorElement.textContent = "";
+  secondInput.textContent = "";
+  firstDigit = a * b;
+  secondDigit = 0;
+  operator = "";
 };
 
 const divide = (a, b) => {
-  return a / b;
+  firstInput.textContent = a / b;
+  operatorElement.textContent = "";
+  secondInput.textContent = "";
+  firstDigit = a / b;
+  secondDigit = 0;
+  operator = "";
 };
 
 const operate = (a, o, b) => {
@@ -32,3 +52,48 @@ const operate = (a, o, b) => {
 var firstDigit = 0;
 var operator = "";
 var secondDigit = 0;
+var focusedInput = "";
+var isSecondInputFlag = false;
+
+var inputDisplay = document.getElementById("input-display");
+var firstInput = document.getElementById("first-input");
+var secondInput = document.getElementById("second-input");
+var operatorElement = document.getElementById("operator");
+
+const digitToInput = (e) => {
+  if (!isSecondInputFlag) {
+    let newValue = `${firstInput.textContent}${e.target.value}`;
+    firstInput.textContent = newValue;
+  } else {
+    let newValue = `${secondInput.textContent}${e.target.value}`;
+    secondInput.textContent = newValue;
+  }
+};
+
+const createOperator = (e) => {
+  operator = e.target.value;
+  firstDigit = parseFloat(firstInput.textContent);
+  operatorElement.textContent = operator;
+  isSecondInputFlag = true;
+};
+
+var buttonsWithValue = document.querySelectorAll(".button--has-value");
+buttonsWithValue.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    digitToInput(e);
+  });
+});
+
+var buttonsWithOperator = document.querySelectorAll(".button--has-operator");
+buttonsWithOperator.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    createOperator(e);
+  });
+});
+
+var equalsButton = document.querySelector(".equals");
+equalsButton.addEventListener("click", (e) => {
+  secondDigit = parseFloat(secondInput.textContent);
+  isSecondInputFlag = false;
+  operate(firstDigit, operator, secondDigit);
+});
