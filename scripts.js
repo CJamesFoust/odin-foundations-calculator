97c91c81df4428943ff8d6f5bb311154707b88bc
CJamesFoust <82835548+CJamesFoust@users.noreply.github.com>
2025-07-29 23:29:01 -0400

    change logic to allow concurrent equal equations like proper calculator. remove inline display of operators. realize percent is broken, will address soon

--- scripts.js
+++ scripts.js
@@ -1,46 +1,38 @@
+// Operation functions
+
 const add = (a, b) => {
-  firstInput.textContent = a + b;
-  operatorElement.textContent = "";
-  secondInput.textContent = "";
+  resultsDisplay.textContent = a + b;
   firstDigit = a + b;
-  secondDigit = 0;
-  operator = "";
+  isFirstOperation = false;
+  justOperated = true;
 };
 
 const subtract = (a, b) => {
-  firstInput.textContent = a - b;
-  operatorElement.textContent = "";
-  secondInput.textContent = "";
+  resultsDisplay.textContent = a - b;
   firstDigit = a - b;
-  secondDigit = 0;
-  operator = "";
+  isFirstOperation = false;
+  justOperated = true;
 };
 
 const multiply = (a, b) => {
-  firstInput.textContent = a * b;
-  operatorElement.textContent = "";
-  secondInput.textContent = "";
+  resultsDisplay.textContent = a * b;
   firstDigit = a * b;
-  secondDigit = 0;
-  operator = "";
+  isFirstOperation = false;
+  justOperated = true;
 };
 
 const divide = (a, b) => {
-  firstInput.textContent = a / b;
-  operatorElement.textContent = "";
-  secondInput.textContent = "";
+  resultsDisplay.textContent = a / b;
   firstDigit = a / b;
-  secondDigit = 0;
-  operator = "";
+  isFirstOperation = false;
+  justOperated = true;
 };
 
 const percent = (a, b) => {
-  firstInput.textContent = (a / 100) * b;
-  operatorElement.textContent = "";
-  secondInput.textContent = "";
+  resultsDisplay.textContent = (a / 100) * b;
   firstDigit = (a / 100) * b;
-  secondDigit = 0;
-  operator = "";
+  isFirstOperation = false;
+  justOperated = true;
 };
 
 const operate = (a, o, b) => {
@@ -56,43 +48,64 @@ const operate = (a, o, b) => {
     case "%":
       return percent(a, b);
     default:
-      return console.error(`Sent value not expected: ${o}`);
+      return console.error(`Sent value not expected: ${o}`, operator);
   }
 };
 
+// Context variables
 var firstDigit = 0;
 var operator = "";
 var secondDigit = 0;
-var focusedInput = "";
 var isSecondInputFlag = false;
+var justOperated = false;
+var isFirstOperation = true;
+var secondInputInit = false;
 
-var inputDisplay = document.getElementById("input-display");
-var firstInput = document.getElementById("first-input");
-var secondInput = document.getElementById("second-input");
-var operatorElement = document.getElementById("operator");
+// Front-end variables
+var resultsDisplay = document.getElementById("results-display");
 
+// Front-end functions
 const digitToInput = (e) => {
-  if (!isSecondInputFlag) {
-    let newValue = `${firstInput.textContent}${e.target.value}`;
-    firstInput.textContent = newValue;
-  } else {
-    let newValue = `${secondInput.textContent}${e.target.value}`;
-    secondInput.textContent = newValue;
-  }
+  if (justOperated === true) {
+    justOperated = false;
+    isSecondInputFlag = true;
+    resultsDisplay.textContent = e.target.value;
+  } else if(secondInputInit === true && isSecondInputFlag === true) {
+    secondInputInit = false;
+      resultsDisplay.textContent = e.target.value;
+    } else {
+        resultsDisplay.textContent = `${resultsDisplay.textContent}${e.target.value}`;
+    }
+
 };
 
 const createOperator = (e) => {
-  operator = e.target.value;
-  firstDigit = parseFloat(firstInput.textContent);
-  operatorElement.textContent = operator;
+  if (isFirstOperation === true) {
+    firstDigit = parseFloat(resultsDisplay.textContent);
+  } else {
+    secondDigit = parseFloat(resultsDisplay.textContent);
+    operate(firstDigit, operator, secondDigit);
+  }
+
   isSecondInputFlag = true;
+  secondInputInit = true;
+  operator = e.target.value;
 };
 
+// Buttons 'Event Listener' functions and assignment
 var buttonsWithValue = document.querySelectorAll(".button--has-value");
 buttonsWithValue.forEach((button) => {
   button.addEventListener("click", (e) => {
     digitToInput(e);
   });
+});
+
+var buttonWithPeriod = document.querySelector(".button--has-period");
+buttonWithPeriod.addEventListener("click", (e) => {
+  if (resultsDisplay.textContent.includes(".")) {
+    return;
+  }
+  digitToInput(e);
 });
 
 var buttonsWithOperator = document.querySelectorAll(".button--has-operator");
@@ -102,11 +115,16 @@ buttonsWithOperator.forEach((button) => {
   });
 });
 
-var equalsButton = document.querySelector(".equals");
+var equalsButton = document.querySelector(".button--equals");
 equalsButton.addEventListener("click", (e) => {
-  secondDigit = parseFloat(secondInput.textContent);
-  isSecondInputFlag = false;
-  operate(firstDigit, operator, secondDigit);
+  if (justOperated === true) {
+    operate(firstDigit, operator, secondDigit);
+  } else {
+    secondDigit = parseFloat(resultsDisplay.textContent);
+    isSecondInputFlag = false;
+    operate(firstDigit, operator, secondDigit);
+  }
+  console.log("in equal function", operator, firstDigit, secondDigit);
 });
 
 var clearButton = document.querySelector(".button--clear");
@@ -114,43 +132,17 @@ clearButton.addEventListener("click", () => {
   firstDigit = 0;
   operator = "";
   secondDigit = 0;
+  isFirstOperation = true;
   isSecondInputFlag = false;
-  operatorElement.textContent = "";
-  firstInput.textContent = "";
-  secondInput.textContent = "";
+  isAdditionalOpFlag = false;
+  justOperated = false;
+  resultsDisplay.textContent = "";
 });
 
 var deleteButton = document.querySelector(".button--delete");
 deleteButton.addEventListener("click", () => {
-  if (isSecondInputFlag === true) {
-    if (secondInput.textContent.length > 1) {
-      secondInput.textContent = secondInput.textContent.substring(
-        0,
-        secondInput.textContent.length - 1
-      );
-    } else if (secondInput.textContent.length == 1) {
-      secondInput.textContent = secondInput.textContent.substring(
-        0,
-        secondInput.textContent.length - 1
-      );
-      isSecondInputFlag = false;
-    } else {
-      isSecondInputFlag = false;
-    }
-  } else if (
-    isSecondInputFlag === false &&
-    operatorElement.textContent.length > 0
-  ) {
-    operatorElement.textContent = "";
-    operator = "";
-  } else if (
-    isSecondInputFlag === false &&
-    operatorElement.textContent.length == 0 &&
-    firstInput.textContent.length > 0
-  ) {
-    firstInput.textContent = firstInput.textContent.substring(
-      0,
-      firstInput.textContent.length - 1
-    );
-  }
+  resultsDisplay.textContent = resultsDisplay.textContent.substring(
+    0,
+    resultsDisplay.textContent.length - 1
+  );
 });
